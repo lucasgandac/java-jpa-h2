5d477c5358be3d625b85b9c22d3ee02b33c62548
lucasgandac <lucasgandac@hotmail.com>
2022-10-25 22:42:14 -0300

    start

---
+++ demo/src/main/java/payroll/PaymentnApp.java
@@ -0,0 +1,14 @@
+package payroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentnApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentnApp.class, args);
+	}
+
+}
+
